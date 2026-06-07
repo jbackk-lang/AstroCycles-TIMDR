9beef9dbacfb68e441eadb00df504352fee16f54
jbackk-lang <jbackk@gmail.com>
2026-06-07 18:05:33 +0200

    Create setup.py for astrocycles_timdr package
    Initial setup configuration for the astrocycles_timdr package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="astrocycles_timdr",
+    version="0.1.0",
+    author="Jacek",
+    description="A geometric model of astrological cycles based on the TIMDR framework.",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    python_requires=">=3.8",
+)
